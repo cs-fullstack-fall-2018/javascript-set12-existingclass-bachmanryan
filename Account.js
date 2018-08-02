8b0997960a362134c26d880747f15eafa2b64bce
Ryan Bachman <jacksprat455@gmail.com>
2018-08-02 11:50:35 -0500

    Classes_Exercise

--- Account.js
+++ Account.js
@@ -23,5 +23,13 @@ class Account{
 }
 
 function main() {
-
+    var ryansAccount = new Account("Ryan's Account", 100);
+    var mattsAccount = new Account("Matt's Account", 1000);
+    var myAccount = new Account("My Account", 0);
+    mattsAccount.withdrawal(100);
+    myAccount.deposit(100);
+    alert("$20 have been added to: " + ryansAccount.accountName +" " + ryansAccount.accountBalance);
+    alert("$100 have been withdrawn from: " + mattsAccount.accountName + " " + mattsAccount.accountBalance);
+    alert("$100 have been deposited into: " + myAccount.accountName + " " + myAccount.accountBalance);
 }
+main();
--- Person.js
+++ Person.js
@@ -22,7 +22,21 @@ class Person {
 }
 
 function main() {
+    var firstPerson = new Person("Dave", 25, 150, 6);
+    var secondPerson = new Person("Fred", 30, 200, 5.5);
 
+    alert(firstPerson.name + "'s BMI is "+ firstPerson.bodyMassIndex());
+    alert(secondPerson.name + "'s BMI is "+ secondPerson.bodyMassIndex());
+    firstPerson.bodyMassIndex();
+    secondPerson.bodyMassIndex();
+    addObjectsToArray(firstPerson, secondPerson);
 }
+function addObjectsToArray(firstPerson, secondPerson) {
+    var newArray = [];
+    newArray.push(firstPerson);
+    newArray.push(secondPerson);
+    newArray[1].printPerson();
+}
+
 
 main();
